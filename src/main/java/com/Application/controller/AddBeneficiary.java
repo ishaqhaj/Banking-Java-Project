bb4f1a86f5eb5492a5aee4380d90b2484387a78e
OuidadM <159729939+OuidadM@users.noreply.github.com>
2024-12-20 17:20:06 +0100

    Update AddBeneficiary.java

--- src/main/java/com/Application/controller/AddBeneficiary.java
+++ src/main/java/com/Application/controller/AddBeneficiary.java
@@ -4,9 +4,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
-import com.Application.dao.impl.AccountDAOImpl;
-import com.Application.dao.impl.UserDAOImpl;
-import com.Application.model.Account;
 import com.Application.model.User;
 import com.Application.service.UserService;
 import com.Application.util.SessionManager;
@@ -21,22 +18,43 @@ public class AddBeneficiary {
     public void addBeneficiary() {
         String name = nameField.getText().trim();
         String iban = ibanField.getText().trim();
+
         if (name.isEmpty() || iban.isEmpty()) {
             Alert alert = new Alert(Alert.AlertType.WARNING);
-            alert.setTitle("Champs manquant");
+            alert.setTitle("Champs manquants");
             alert.setHeaderText(null);
             alert.setContentText("Veuillez remplir tous les champs.");
             alert.showAndWait();
             return;
         }
+
         UserService userService = new UserService();
         User authenticatedUser = SessionManager.getInstance().getAuthenticatedUser();
-        boolean success = userService.addBeneficiary(authenticatedUser.getUserId(), name, iban);
 
-        if (success) {
-            showSuccess("Le bénéficiaire a été ajouté avec succès.");
+        try {
+            boolean success = userService.addBeneficiary(authenticatedUser.getUserId(), name, iban);
+
+            if (success) {
+                showSuccess("Le bénéficiaire a été ajouté avec succès.");
+            } else {
+                if(userService.isBeneficiaryExists(SessionManager.getInstance().getSelectedAccount().getAccountNumber(),iban)){
+                    Alert alert = new Alert(Alert.AlertType.WARNING);
+                    alert.setTitle("Bénéficiaire existant");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Ce bénéficiaire a déjà été ajouté.");
+                    alert.showAndWait();
+                }
             }
+        } catch (IllegalArgumentException e) {
+            // Afficher une alerte contenant le message de l'exception
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Erreur - Bénéficiaire invalide");
+            alert.setHeaderText(null);
+            alert.setContentText(e.getMessage());
+            alert.showAndWait();
         }
+    }
+
     private void showSuccess(String message) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Succès");
