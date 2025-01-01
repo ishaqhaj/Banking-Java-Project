5bd24070477495c92e6c1f0f6a7b4a1ebd4ad171
ishaqhaj <162517752+ishaqhaj@users.noreply.github.com>
2025-01-01 18:39:01 +0100

    Mise à jour Accueil.java après le test de sonarqube

--- src/main/java/com/Application/controller/Accueil.java
+++ src/main/java/com/Application/controller/Accueil.java
@@ -1,87 +1,83 @@
 package com.Application.controller;
 
-import com.Application.dao.UserDAO;
-import com.Application.model.Virement;
-import com.Application.service.UserService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
-import com.Application.dao.impl.AccountDAOImpl;
-import com.Application.dao.impl.UserDAOImpl;
-import com.Application.model.Account;
-import com.Application.model.User;
-import com.Application.util.SessionManager;
+
+import com.application.dao.impl.AccountDAOImpl;
+import com.application.model.Account;
+import com.application.model.User;
+import com.application.util.SessionManager;
+
 import lombok.Getter;
 import lombok.Setter;
 
 import java.io.IOException;
-import java.math.BigDecimal;
-import java.util.Map;
+import java.util.logging.*;
 
 public class Accueil {
     @Getter
     @Setter
-    private static BorderPane statiqueMainApp; // Champ statique
 
     @FXML
     private BorderPane mainPane; // Champ temporaire pour l'injection FXML
+    Accueil accueilController;
+    private final static String selectAccountInt="/gui/SelectAccount.fxml";
+    private final static String selectBeneficiaryInt="/gui/SelectBeneficiary.fxml";
+    
+    private static final Logger LOGGER = Logger.getLogger(Accueil.class.getName());
 
     @FXML
     public void initialize() {
         // Initialisation manuelle de la variable statique
-        statiqueMainApp = mainPane;
         SessionManager.getInstance().registerController("Accueil", this);
-        loadPage("/GUI/history.fxml");
+        accueilController = (Accueil) SessionManager.getInstance().getController("Accueil");
+        loadPage("/gui/history.fxml");
     }
     @FXML
     public void addAccount() {
         // Implémentation pour ajouter un compte
-        loadPage("/GUI/AddAccount.fxml");
+        loadPage("/gui/AddAccount.fxml");
     }
     @FXML
     public void selectAccount(ActionEvent event) {
-        UserService userService = new UserService();
         User authenticatedUser = SessionManager.getInstance().getAuthenticatedUser();
         AccountDAOImpl accountDAO = new AccountDAOImpl();
-        UserDAO userDAO = new UserDAOImpl();
         if(accountDAO.hasSingleAccount(authenticatedUser.getUserId())){
             SessionManager.getInstance().setSelectedAccount(accountDAO.getUserAccount(authenticatedUser.getUserId()));
             addBeneficiary();
         }
         else{
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/SelectAccount.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(selectAccountInt));
             try {
                 Pane selectAccountPane = loader.load();
                 SelectAccount selectAccountController = loader.getController();
                 // Définir la redirection personnalisée après sélection
-                selectAccountController.setOnAccountSelected(selectedAccount -> {
+                selectAccountController.setOnAccountSelected(selectedAccount -> 
                     // Rediriger vers l'ajout de bénéficiaire
-                    addBeneficiary();
-                });
+                    addBeneficiary()
+                );
                 // Charger le contenu dans la fenêtre principale
-                statiqueMainApp.setCenter(selectAccountPane);
+                accueilController.getMainPane().setCenter(selectAccountPane);
 
             } catch (IOException e) {
-                e.printStackTrace();
+            	LOGGER.log(Level.SEVERE, "Erreur lors du traitement", e);
             }
 
         }
     }
     public void addBeneficiary() {
-        loadPage("/GUI/Addbeneficiary.fxml");
+        loadPage("/gui/Addbeneficiary.fxml");
     }
     @FXML
     public void virementSimple() {
-        UserDAOImpl userDAO = new UserDAOImpl();
         AccountDAOImpl accountDAO = new AccountDAOImpl();
         User authenticatedUser = SessionManager.getInstance().getAuthenticatedUser();
 
         if (!accountDAO.hasSingleAccount(authenticatedUser.getUserId())) {
-            loadPage("/GUI/SelectAccount.fxml");
+            loadPage(selectAccountInt);
             return; // Arrêtez ici pour attendre que l'utilisateur sélectionne un compte
         }
 
@@ -94,27 +90,26 @@ public class Accueil {
 
     @FXML
     public void virementDeMasse(ActionEvent event) {
-        UserDAOImpl userDAO = new UserDAOImpl();
         AccountDAOImpl accountDAO = new AccountDAOImpl();
         User authenticatedUser = SessionManager.getInstance().getAuthenticatedUser();
 
         if (!accountDAO.hasSingleAccount(authenticatedUser.getUserId())) {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/SelectAccount.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(selectAccountInt));
             try {
                 Pane selectAccountPane = loader.load();
                 SelectAccount selectAccountController = loader.getController();
                 // Définir la redirection personnalisée après sélection
-                selectAccountController.setOnAccountSelected(selectedAccount -> {
+                selectAccountController.setOnAccountSelected(selectedAccount -> 
                     // Rediriger vers l'ajout de bénéficiaire
-                    navigateAfterAccountSelectionMasse();
-                });
+                    navigateAfterAccountSelectionMasse()
+                );
                 // Charger le contenu dans la fenêtre principale
-                statiqueMainApp.setCenter(selectAccountPane);
+                accueilController.getMainPane().setCenter(selectAccountPane);
+                
 
             } catch (IOException e) {
-                e.printStackTrace();
+            	LOGGER.log(Level.SEVERE, "Erreur lors du traitement du virement de masse.", e);
             }
-            return;
         }
         // Si l'utilisateur n'a qu'un seul compte, on le sélectionne automatiquement
         else {
@@ -126,25 +121,24 @@ public class Accueil {
 
     @FXML
     public void virementMultiple(ActionEvent event){
-        UserDAOImpl userDAO = new UserDAOImpl();
         AccountDAOImpl accountDAO = new AccountDAOImpl();
         User authenticatedUser = SessionManager.getInstance().getAuthenticatedUser();
 
         if (!accountDAO.hasSingleAccount(authenticatedUser.getUserId())) {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/SelectAccount.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(selectAccountInt));
             try {
                 Pane selectAccountPane = loader.load();
                 SelectAccount selectAccountController = loader.getController();
                 // Définir la redirection personnalisée après sélection
-                selectAccountController.setOnAccountSelected(selectedAccount -> {
+                selectAccountController.setOnAccountSelected(selectedAccount -> 
                     // Rediriger vers l'ajout de bénéficiaire
-                    navigateAfterAccountSelectionMultiple();
-                });
+                    navigateAfterAccountSelectionMultiple()
+                );
                 // Charger le contenu dans la fenêtre principale
-                statiqueMainApp.setCenter(selectAccountPane);
-
+                accueilController.getMainPane().setCenter(selectAccountPane);
+                
             } catch (IOException e) {
-                e.printStackTrace();
+            	LOGGER.log(Level.SEVERE, "Erreur lors du traitement du virement multiple.", e);
             }
             return; // Arrêtez ici pour attendre que l'utilisateur sélectionne un compte
         }
@@ -157,71 +151,77 @@ public class Accueil {
     }
 
     public void navigateAfterAccountSelectionSimple() {
-        loadPage("/GUI/SelectBeneficiary.fxml");
+        loadPage(selectBeneficiaryInt);
     }
     public void navigateAfterAccountSelectionMultiple() {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/SelectBeneficiary.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(selectBeneficiaryInt));
         try {
             Pane selectBeneficiaryPane = loader.load();
             SelectBeneficiary selectBeneficiaryController = loader.getController();
             // Définir la redirection personnalisée après sélection
-            selectBeneficiaryController.setRedirection(selectedBeneficiary -> {
+            selectBeneficiaryController.setRedirection(selectedBeneficiary ->
                 // Rediriger vers l'ajout de bénéficiaire
-                passerVirementMultiple();
-            });
+                passerVirementMultiple()
+            );
             // Charger le contenu dans la fenêtre principale
-            statiqueMainApp.setCenter(selectBeneficiaryPane);
+            accueilController.getMainPane().setCenter(selectBeneficiaryPane);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
     public void navigateAfterAccountSelectionMasse(){
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/SelectBeneficiary.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(selectBeneficiaryInt));
         try {
             Pane selectBeneficiaryPane = loader.load();
             SelectBeneficiary selectBeneficiaryController = loader.getController();
             // Définir la redirection personnalisée après sélection
-            selectBeneficiaryController.setRedirection(selectedBeneficiary -> {
+            selectBeneficiaryController.setRedirection(selectedBeneficiary ->
                 // Rediriger vers l'ajout de bénéficiaire
-                passerVirementMasse();
-            });
+                passerVirementMasse()
+            );
             // Charger le contenu dans la fenêtre principale
-            statiqueMainApp.setCenter(selectBeneficiaryPane);
-
+            accueilController.getMainPane().setCenter(selectBeneficiaryPane);
+            
         } catch (IOException e) {
-            e.printStackTrace();
+        	LOGGER.log(Level.SEVERE, "Erreur lors du traitement du virement de masse.", e);
         }
     }
     public void passerVirementSimple(){
-        loadPage("/GUI/VirementSimple.fxml");
+        loadPage("/gui/VirementSimple.fxml");
     }
     public void passerVirementMultiple(){
-        loadPage("/GUI/VirementMultiple.fxml");
+        loadPage("/gui/VirementMultiple.fxml");
     }
 
     public void passerVirementMasse(){
-        loadPage("/GUI/VirementMasse.fxml");
+        loadPage("/gui/VirementMasse.fxml");
     }
 
     public void loadPage(String fxmlFile) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));
             Pane newContent = loader.load();
-
             // Remplacer uniquement le contenu central
-            if (statiqueMainApp != null) {
-                statiqueMainApp.setCenter(newContent);
+            if (accueilController.getMainPane() != null) {
+            	accueilController.getMainPane().setCenter(newContent);
            } else {
-                System.err.println("Erreur : mainPane est null. Vérifiez l'initialisation.");
-            }
+               LOGGER.severe("Erreur : mainPane est null. Vérifiez l'initialisation.");
+           }
 
         } catch (IOException e) {
-            e.printStackTrace();
+        	LOGGER.log(Level.SEVERE, "Erreur lors du chargement de la page : " + fxmlFile, e);
         }
     }
 
     public void loadHistory(){
-        loadPage("/GUI/history.fxml");
+        loadPage("/gui/history.fxml");
     }
+    
+	public BorderPane getMainPane() {
+		return mainPane;
+	}
+	
+    
+    
 }
