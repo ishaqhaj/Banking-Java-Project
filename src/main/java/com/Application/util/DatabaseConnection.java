a13bb0e64ad1281cb4cf717ee336221b09895ce1
Chaimaefadil <158011383+Chaimaefadil@users.noreply.github.com>
2025-01-01 21:43:34 +0100

    Mise à jour de DatabaseConnection.java après le test avec SonarQube

--- src/main/java/com/Application/util/DatabaseConnection.java
+++ src/main/java/com/Application/util/DatabaseConnection.java
@@ -1,9 +1,7 @@
 package com.Application.util;
 
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.util.Properties;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
