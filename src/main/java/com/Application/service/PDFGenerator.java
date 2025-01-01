63ac4972c8626c5b050fb3d5f2a8b23d9c1f54d3
elmouktadir <165966942+elmouktadir@users.noreply.github.com>
2025-01-01 21:34:00 +0100

    Mise à jour de Update PDFGenerator.java après le test avec SonarQube

--- src/main/java/com/Application/service/PDFGenerator.java
+++ src/main/java/com/Application/service/PDFGenerator.java
@@ -1,5 +1,7 @@
 package com.Application.service;
 
+import com.Application.model.Account;
+import com.Application.model.Address;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Paragraph;
@@ -7,12 +9,17 @@ import com.itextpdf.text.pdf.PdfWriter;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PDFGenerator {
+	private static final Logger LOGGER = Logger.getLogger(PDFGenerator.class.getName());
+	// Constructeur privé pour empêcher l'instanciation
+    private PDFGenerator() {
+        throw new UnsupportedOperationException("Cette classe ne peut pas être instanciée.");
+    }
 
-    public static String generateUserPdf(String name, String email, String idValue, String address, String city,
-                                         String postalCode, String country, String accountNumber,
-                                         String accountType, String bank, String bic) {
+    public static String generateUserPdf(String name, String email, String idValue, Address address, Account account) {
         String filePath = "user_receipt.pdf";
         Document document = new Document();
 
@@ -24,17 +31,17 @@ public class PDFGenerator {
             document.add(new Paragraph("Nom: " + name));
             document.add(new Paragraph("Email: " + email));
             document.add(new Paragraph("CIN ou Passeport: " + idValue));
-            document.add(new Paragraph("Adresse: " + address));
-            document.add(new Paragraph("Ville: " + city));
-            document.add(new Paragraph("Code postal: " + postalCode));
-            document.add(new Paragraph("Pays: " + country));
-            document.add(new Paragraph("Numéro de compte: " + accountNumber));
-            document.add(new Paragraph("Type de compte: " + accountType));
-            document.add(new Paragraph("Banque: " + bank));
-            document.add(new Paragraph("Code BIC: " + bic));
+            document.add(new Paragraph("Adresse: " + address.getAddress()));
+            document.add(new Paragraph("Ville: " + address.getCity()));
+            document.add(new Paragraph("Code postal: " + address.getPostalCode()));
+            document.add(new Paragraph("Pays: " + address.getCountry()));
+            document.add(new Paragraph("Numéro de compte: " + account.getAccountNumber()));
+            document.add(new Paragraph("Type de compte: " + account.getAccountType()));
+            document.add(new Paragraph("Banque: " + account.getBank().getName()));
+            document.add(new Paragraph("Code BIC: " + account.getBank().getBic()));
 
         } catch (DocumentException | IOException e) {
-            e.printStackTrace();
+        	LOGGER.log(Level.SEVERE, "Erreur lors de la génération du reçu.", e);
             return null;
         } finally {
             document.close();
