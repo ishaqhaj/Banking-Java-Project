bf68e45947c934ea9e5d75c6188170c237770685
OuidadM <159729939+OuidadM@users.noreply.github.com>
2025-01-01 21:24:32 +0100

    Add files via upload
    -Ajout d'une classe Address afin de diminuer le nombres de paramètres fournis au constructeur de User. (Suivant les suggestions du résultat du test de SonarQube).
    -Modification de application.properties pou sauvegarder le mot de passe de l'application de l'email avec la classe ConfigLoader. (Suivant les améliorations de la partie de sécurité du code).

---
+++ src/main/java/com/Application/model/Address.java
@@ -0,0 +1,27 @@
+package com.application.model;
+
+public class Address{
+	private String addressString;
+    private String city;
+    private String postalCode;
+    private String country;
+    public Address(String address,String city,String postalCode,String country) {
+    	this.addressString=address;
+    	this.city=city;
+    	this.postalCode=postalCode;
+    	this.country=country;
+    }
+    public String getAddress() {
+    	return addressString;
+    }
+    public String getCity() {
+    	return city;
+    }
+    public String getPostalCode() {
+    	return postalCode;
+    }
+    public String getCountry() {
+    	return country;
+    }
+       
+}
---
+++ src/main/java/com/Application/util/ConfigLoader.java
@@ -0,0 +1,25 @@
+package com.application.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigLoader {
+    private static final Properties properties = new Properties();
+
+    static {
+        try (InputStream input = ConfigLoader.class.getClassLoader().getResourceAsStream("application.properties")) {
+            if (input == null) {
+                throw new RuntimeException("Configuration file not found");
+            }
+            properties.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load configuration file.", e);
+        }
+    }
+
+    public static String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+}
+
